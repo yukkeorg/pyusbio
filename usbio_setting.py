b6f820ba200d7e79fc3465ddbfdb79f7cee0b4dd
Yusuke Ohshima <git@yukke.org>
2011-09-25 22:26:19 +0900

    add gathering and display USB--IO status option

--- usbio_setting.py
+++ usbio_setting.py
@@ -25,6 +25,7 @@ def toInt(val):
 
 def main():
   parser = OptionParser()
+  parser.add_option("--show", action="store_true", dest="show", help="Display USB-IO System config.")
   parser.add_option("--enable-pullup", action="store_true", dest="is_pullup", help="Enable Port1 pullup.")
   parser.add_option("--disable-pullup", action="store_false", dest="is_pullup", help="Disable Port1 pullup.")
   parser.add_option("--port1", dest="port1", help="Port1 pin setting. 0bxxxxxxxx [7..0], x=[0:Out, 1:In]")
@@ -43,8 +44,17 @@ def main():
     print("USB-IO is not found.")
     sys.exit(1)
 
-  oldsc = dev.getSysConf()
-  sc = oldsc.copy()
+  _sc = dev.getSysConf()
+  if opts.show:
+    print("Port2 Pullup : {0}".format("Enable" if _sc.is_pullup else "Disable"))
+    print("Port1 Pin setting : {0:0>8b}(0x{0:02x})".format(_sc.port1))
+    print("Port2 Pin setting : {0:0>8b}(0x{0:02x})".format(_sc.port2))
+    print("Port1 Pin init value : {0:0>8b}(0x{0:02x})".format(_sc.init1))
+    print("Port2 Pin init value : {0:0>8b}(0x{0:02x})".format(_sc.init2))
+    sys.exit(0)
+    
+    
+  sc = _sc.copy()
   if not opts.is_pullup is None:
     sc.is_pullup = opts.is_pullup 
   sc.port1 = toInt(opts.port1) if opts.port1 is not None else sc.port1
@@ -53,8 +63,8 @@ def main():
   sc.init2 = toInt(opts.init2) if opts.init2 is not None else sc.init2
   dev.setSysConf(sc)
 
-  print "Setting Complete. "
-  print "NOTICE:If you use this settings, You most reconnect USB-IO from your PC."
+  print("Setting Complete. ")
+  print("NOTICE:If you use this settings, You most reconnect USB-IO from your PC.")
 
 
 if __name__ == '__main__':
