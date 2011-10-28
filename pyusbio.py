e8f161cd4503dde8ddc8372e7550d2f1d5631e1b
Yusuke Ohshima <git@yukke.org>
2011-10-29 02:50:55 +0900

    Stable release

--- pyusbio.py
+++ pyusbio.py
@@ -1,7 +1,36 @@
-#!/usr/bin/env python
+# coding: utf-8
 
+# Copyright (c) 2011, Yusuke Ohshima
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+# 
+#   - Redistributions of source code must retain the above copyright notice, 
+#     this list of conditions and the following disclaimer.
+# 
+#   - Redistributions in binary form must reproduce the above copyright notice, 
+#     this list of conditions and the following disclaimer in the documentation 
+#     and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
 import usb.core
 import usb.util
+
+import logging
+logger = logging.getLogger('USBIO')
+
 
 # Device Information
 VENDOR_ID = 0x1352       # Km2Net
@@ -9,6 +38,7 @@ PRODUCT_ID_ORIG = 0x0120 # USB-IO2.0 ORIGINAL
 PRODUCT_ID_AKI = 0x0121  # USB-IO2.0(AKI)
 
 # Command list
+# Detail to see : http://km2net.com/usb-fsio/command.shtml
 CMD_READ_SEND = 0x20
 CMD_SEND_READ = 0x21
 CMD_SEND_REPEAT = 0x22
@@ -17,17 +47,18 @@ CMD_FLASHROM_WRITE = 0xf1
 CMD_SYSCONF_READ = 0xf8
 CMD_SYSCONF_WRITE = 0xf9
 
-TIMEOUT = 5000
 MAX_CMD_LENGTH = 64
 
 
 class USBIO(object):
-  def __init__(self):
+  def __init__(self, timeout=5000):
     self.device = None
     self.is_aki = False
     self.inEpAddr = 0
     self.outEpAddr = 0
     self._seq = 0
+    self._timeout = timeout
+
 
   def find_and_init(self):
     for product_id in (PRODUCT_ID_ORIG, PRODUCT_ID_AKI):
@@ -35,10 +66,10 @@ class USBIO(object):
       if self.device:
         if product_id == PRODUCT_ID_AKI:
           self.is_aki = True
-        print("USB-IO(0x{0:x}, 0x{1:x}) is found.".format(VENDOR_ID, product_id))
+        logger.info("USB-IO(0x{0:x}, 0x{1:x}) is found.".format(VENDOR_ID, product_id))
         break
     if not self.device:
-      print("USB-IO Device is NOT found.")
+      logger.error("USB-IO Device is NOT found.", file=sys.stderr)
       return False
 
     for cnf in self.device:
@@ -46,16 +77,16 @@ class USBIO(object):
         for ep in ifc:
           if ep.bEndpointAddress & usb.util._ENDPOINT_DIR_MASK:
             self.inEpAddr = ep.bEndpointAddress
-            print("IN endpoint address of interface {0} : 0x{1:x}" \
+            logger.debug("IN endpoint address of interface {0} : 0x{1:x}" \
                    .format(ifc.bInterfaceNumber, ep.bEndpointAddress))
           else:
             self.outEpAddr = ep.bEndpointAddress
-            print("OUT endpoint address of interface {0} : 0x{1:x}" \
+            logger.debug("OUT endpoint address of interface {0} : 0x{1:x}" \
                    .format(ifc.bInterfaceNumber, ep.bEndpointAddress))
 
         if self.device.is_kernel_driver_active(ifc.bInterfaceNumber):
           self.device.detach_kernel_driver(ifc.bInterfaceNumber)
-          print("interface {0} of USB-IO was detached from the kernel driver." \
+          logger.info("interface {0} of USB-IO was detached from the kernel driver." \
                    .format(ifc.bInterfaceNumber))
     return True
 
@@ -73,12 +104,12 @@ class USBIO(object):
       for i in xrange(length):
         cmd[i+1] = writedata[i]
 
-    sendsize = self.device.write(self.outEpAddr, cmd, timeout=TIMEOUT)
+    sendsize = self.device.write(self.outEpAddr, cmd, timeout=self._timeout)
     if sendsize != cmdsize:
       return False
 
     if do_read:
-      data = self.device.read(self.inEpAddr, cmdsize, timeout=TIMEOUT)
+      data = self.device.read(self.inEpAddr, cmdsize, timeout=self._timeout)
       if data[0] != cmd[0] or data[cmdsize-1] != cmd[cmdsize-1]:
         raise ValueError, "Different recived data."
       return data[1:cmdsize-1]
@@ -89,14 +120,18 @@ class USBIO(object):
   def send2read(self, setdata=None):
     return (self._cmd(CMD_READ_SEND, writedata=setdata))[0:2]
 
+
   def getSysConf(self):
     sc = SysConf()
     return sc.fromArray(self._cmd(CMD_SYSCONF_READ))
+
 
   def setSysConf(self, sysconf):
     if not isinstance(sysconf, SysConf):
       raise TypeError, "sysconf is not SysConf type."
     return self._cmd(CMD_SYSCONF_WRITE, writedata=sysconf.toArray(), do_read=False)
+
+
 
 
 class SysConf(object):
@@ -107,6 +142,7 @@ class SysConf(object):
     self.init1 = init1
     self.init2 = init2
 
+
   def toArray(self):
     data = [0] * (MAX_CMD_LENGTH - 2)
     data[1] = 0 if self.is_pullup else 1
@@ -116,6 +152,7 @@ class SysConf(object):
     data[9] = self.init2
     return data
 
+
   def fromArray(self, data):
     self.is_pullup = (data[1] == 0)
     self.port1 = data[4]
@@ -124,8 +161,10 @@ class SysConf(object):
     self.init2 = data[9]
     return self
 
+
   def copy(self):
     return SysConf(self.is_pullup, self.port1, self.port2, self.init1, self.init2)
+
 
   def __repr__(self):
     return "<SysConf is_pullup={is_pullup}, portIO={portIO}, init={init}>".format(**self.__dict__)
--- usbio_config.py
+++ usbio_config.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Copyright (c) 2011, Yusuke Ohshima
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+# 
+#   - Redistributions of source code must retain the above copyright notice, 
+#     this list of conditions and the following disclaimer.
+# 
+#   - Redistributions in binary form must reproduce the above copyright notice, 
+#     this list of conditions and the following disclaimer in the documentation 
+#     and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THE POSSIBILITY OF SUCH DAMAGE.
+
 import sys
 from optparse import OptionParser
 from pyusbio import USBIO
@@ -30,8 +54,8 @@ def main():
   parser.add_option("--disable-pullup", action="store_false", dest="is_pullup", help="Disable Port1 pullup.")
   parser.add_option("--port1", dest="port1", help="Port1 pin setting. 0bxxxxxxxx [7..0], x=[0:Out, 1:In]")
   parser.add_option("--port2", dest="port2", help="Port2 pin setting. 0b0000xxxx [3..0], x=[0:Out, 1:In]")
-  parser.add_option("--init1", dest="init1", help="Port1 initilaize value. (ORIG only)")
-  parser.add_option("--init2", dest="init2", help="Port2 initialize value. (ORIG only)")
+  parser.add_option("--init1", dest="init1", help="Port1 default value. (ORIG only)")
+  parser.add_option("--init2", dest="init2", help="Port2 default value. (ORIG only)")
 
   if len(sys.argv) < 2:
     parser.print_help()
@@ -49,8 +73,8 @@ def main():
     print("Port2 Pullup : {0}".format("Enable" if _sc.is_pullup else "Disable"))
     print("Port1 Pin setting : {0:0>8b}(0x{0:02x})".format(_sc.port1))
     print("Port2 Pin setting : {0:0>8b}(0x{0:02x})".format(_sc.port2))
-    print("Port1 Pin init value : {0:0>8b}(0x{0:02x})".format(_sc.init1))
-    print("Port2 Pin init value : {0:0>8b}(0x{0:02x})".format(_sc.init2))
+    print("Port1 Pin default value : {0:0>8b}(0x{0:02x})".format(_sc.init1))
+    print("Port2 Pin default value : {0:0>8b}(0x{0:02x})".format(_sc.init2))
     sys.exit(0)
     
     
@@ -64,7 +88,7 @@ def main():
   dev.setSysConf(sc)
 
   print("Setting Complete. ")
-  print("NOTICE:If you use this settings, You most reconnect USB-IO from your PC.")
+  print("NOTICE:If you apply completely this settings, You most reconnect USB-IO module from your PC.")
 
 
 if __name__ == '__main__':
